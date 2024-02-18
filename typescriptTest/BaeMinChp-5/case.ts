b4a459b558eb53e456751e9a6942da75d5b7737c
SUHONG\topic <topic828@Naver.com>
2024-02-18 22:08:45 +0900

    infer타입

--- typescriptTest/BaeMinChp-5/case.ts
+++ typescriptTest/BaeMinChp-5/case.ts
@@ -17,3 +17,8 @@ interface Card {
 type PayMethod<T> = T extends 'card' ? Card : Bank;
 type CardPayMethodType = PayMethod<'card'>;
 type BankMethodType = PayMethod<'bank'>;
+
+type UnpackPromise<T> = T extends Promise<infer K>[] ? K : any;
+
+const promises = [Promise.resolve('Mark'), Promise.resolve(38)];
+type Expected = UnpackPromise<typeof promises>; //type Expected = string | number
