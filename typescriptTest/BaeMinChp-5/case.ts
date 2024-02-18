65e040188a5c50f83861270873a6d9b41136aa49
SUHONG\topic <topic828@Naver.com>
2024-02-18 22:18:59 +0900

    템플릿 리터럴

--- typescriptTest/BaeMinChp-5/case.ts
+++ typescriptTest/BaeMinChp-5/case.ts
@@ -22,3 +22,16 @@ type UnpackPromise<T> = T extends Promise<infer K>[] ? K : any;
 
 const promises = [Promise.resolve('Mark'), Promise.resolve(38)];
 type Expected = UnpackPromise<typeof promises>; //type Expected = string | number
+
+type Direction =
+  | 'top'
+  | 'topLeft'
+  | 'topRight'
+  | 'bottom'
+  | 'bottomLeft'
+  | 'bottomRight';
+
+type Vertical = 'top' | 'bottom';
+type Horizon = 'left' | 'right';
+
+type Direction2 = Vertical | `${Vertical}${Capitalize<Horizon>}`;
