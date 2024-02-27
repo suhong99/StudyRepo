78b304bbc4fa4e11adfa777f849803d8c2f0aba4
SUHONG\topic <topic828@Naver.com>
2024-02-27 19:38:53 +0900

    모킹공부

---
+++ typescriptTest/type-study/src/Chp7/service/mocking.ts
@@ -0,0 +1,45 @@
+import { resolve } from "path";
+
+const mockFetchBrands = (): Promise<FetchBrandsResponse> =>
+  new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({
+        status: 'SUCCESS',
+        message: null,
+        data: [
+          {
+            id: 1,
+            label: '배민스토어',
+          },
+          {
+            id: 2,
+            label: '비마트',
+          },
+        ],
+      });
+    }, 500);
+  });
+
+const fetchBrands = () => {
+  if (useMock) {
+    return mockFetchBrands();
+  }
+  return requester.get('/brands');
+};
+//
+const useMock = Object.is(REACT_APP_MOCK, 'true');
+const mockFn = ({status=200,time=100,use=true}:MockResult) => use &&
+mock.onGet(/\/order\/list/).reply(()=>
+    new Promise((resolve)=>
+    setTimeout(()=>{
+        resolve([
+            status,
+            status ===200? fetchOrderListSuccessResponse : undefined,
+        ]);
+    },time)
+    )
+
+    if(useMock){
+        mockFn({status:200,time:100,use:true})
+    }
+)
