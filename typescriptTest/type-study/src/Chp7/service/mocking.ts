2da03a544f71b99e12ca5a9819f84ceb607ea4a4
SUHONG\topic <topic828@Naver.com>
2024-03-01 16:10:21 +0900

    모킹

--- typescriptTest/type-study/src/Chp7/service/mocking.ts
+++ typescriptTest/type-study/src/Chp7/service/mocking.ts
@@ -29,7 +29,7 @@ const fetchBrands = () => {
 //
 const useMock = Object.is(REACT_APP_MOCK, 'true');
 const mockFn = ({status=200,time=100,use=true}:MockResult) => use &&
-mock.onGet(/\/order\/list/).reply(()=>
+    mock.onGet(/\/order\/list/).reply(()=>
     new Promise((resolve)=>
     setTimeout(()=>{
         resolve([
