fc68fd8d02ec7cbcc91d2f3f064e9d025a3f818c
suhong99 <topic828@naver.com>
2025-04-11 20:28:51 +0900

    이미지 확대 효과

--- framer/src/component/example/Scroll/tossclone/components/FloatingCard.tsx
+++ framer/src/component/example/Scroll/tossclone/components/FloatingCard.tsx
@@ -1,15 +1,12 @@
 import { motion, useScroll, useTransform } from "framer-motion";
-import { useRef } from "react";
 
 const FloatingCard = () => {
-  const ref = useRef(null);
-
   const { scrollY } = useScroll();
-  const opacity = useTransform(scrollY, [0, 1000, 2200], [0.3, 0.7, 0]);
-
+  const opacity = useTransform(scrollY, [1300, 2200], [0.7, 0]);
+  const scale = useTransform(scrollY, [0, 1200], [1, 1.3]);
+  const blackOpacity = useTransform(scrollY, [0, 1000], [0.2, 0.6]);
   return (
     <motion.div
-      ref={ref}
       style={{
         width: "100%",
         height: "100vh",
@@ -17,14 +14,44 @@ const FloatingCard = () => {
         opacity,
         position: "fixed",
         top: "0px",
-        zIndex: 10,
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
-        color: "white",
+        flexDirection: "column",
+        zIndex: 2,
+        pointerEvents: "none",
+        overflow: "hidden",
       }}
     >
-      FloatingCard
+      <motion.img
+        src="/partenon.jpg"
+        alt="Floating img"
+        width="100%"
+        height="100%"
+        style={{ scale, position: "absolute", top: "0px" }}
+      />
+      <motion.div
+        style={{
+          position: "absolute",
+          width: "100%",
+          height: "100%",
+          backgroundColor: "black",
+          opacity: blackOpacity,
+        }}
+      />
+      <motion.div
+        style={{
+          zIndex: 3,
+          color: "red",
+          fontSize: "3rem",
+          textAlign: "center",
+        }}
+      >
+        {"당신도 깊게 몰입했던 무언가가 있나요?"}
+        {"첫 번쨰 줄이 올라옵니다"}
+        {"두 번째 줄이 올라옵니다"}
+        {"세 번째 줄이 올라옵니다"}
+      </motion.div>
     </motion.div>
   );
 };
